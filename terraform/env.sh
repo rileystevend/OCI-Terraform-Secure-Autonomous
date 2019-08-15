2eac029b482b80501ee217730ed72c0789d27469
Steven Riley <rileystevend@gmail.com>
2019-08-15 14:42:34 -0500

    orchestration finished for user2

--- terraform/env.sh
+++ terraform/env.sh
@@ -7,9 +7,9 @@ export TF_VAR_region="us-ashburn-1"
 #Enter your user OCID
 export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaapojltszogsvqwohv6c6zofuhahp754qwgospfamkjefkfn37x24a"
 #Fingerprint
-export TF_VAR_fingerprint=""
+export TF_VAR_fingerprint="59:a2:af:2f:cd:eb:a1:a2:85:82:67:88:db:fc:db:dc"
 #Developer image ocid
-export TF_VAR_developer_image_ocid="ocid1.image.oc1..aaaaaaaanqr276n5ia2hgmjr25gvdn3ejtok4apli2yecbqobmkyve5v47mq"
+export TF_VAR_developer_image_ocid="ocid1.image.oc1..aaaaaaaaaoswevgnrsoux7lfqbxgtng4aruphm2nbwpoqs22xx7anoi5cikq"
 #Path to your private API key
 export TF_VAR_oci_private_key="../.oci/oci_api_key.pem"
 #Path to your public API key
